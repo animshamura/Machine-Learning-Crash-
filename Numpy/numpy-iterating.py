dc0ca7b5aedc5e6a7a9c4b98bee5e0aaf0d1adff
Anim <47661479+radiant-fleak@users.noreply.github.com>
2023-08-09 00:22:53 +0600

    Create numpy-iterating.py

---
+++ Numpy/numpy-iterating.py
@@ -0,0 +1,5 @@
+import numpy as numby
+numb = numby.array([['numpy','pandas'],['tkinter','pytorch'],['tensorflow','scifi'],['lightgbm','theano']])
+for x in numb :
+    for i in x: 
+         print(i)
